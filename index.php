d35038d4aa97d7b69df6d9ffca58b948787a4f63
Olek Lipiec <34983357+Fanfaryy@users.noreply.github.com>
2020-10-27 12:02:03 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <?php
+
+    require_once("lib/connect.php");
+
+    $query = "SELECT * FROM `lib_autor`";
+    $result = $conn->query($query);
+
+    while ($rs = $result->fetch_assoc()) {
+        echo("ID ".$row['name']."<br>");
+    }
+
+    $conn->close();
+
+    echo("start");
+
+    ?>
+    
+</body>
+</html>
